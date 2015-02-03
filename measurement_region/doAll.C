17a374d58fab3bcaf46e1e302f3f2e3a4d41f286
iandyckes <iandyckes@gmail.com>
2015-02-03 15:13:25 -0800

    fix nfs location for fakeratelooper and measurement_region/doall

--- measurement_region/doAll.C
+++ measurement_region/doAll.C
@@ -7,7 +7,7 @@
 
   TChain *qcd = new TChain("t"); 
   //  qcd->Add( Form("../output/%s/qcd1.root",tag.c_str()) );
-  qcd->Add( Form("../output/%s/qcd1.root",tag.c_str()) );
+  qcd->Add( Form("../fake_rate_output/%s/qcd1.root",tag.c_str()) );
 
   // TChain *ttbar = new TChain("t"); 
   // ttbar->Add("/home/users/iandyckes/CMSSW_5_3_18/ss/ss2015/babymaker/ttbar1.root");
